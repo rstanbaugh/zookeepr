40e9bf1b5cdcb986278a6a776aa4cc3ce9c3a638
rstanbaugh <rich_bootcamp@icloud.com>
2022-06-10 07:32:01 -0400

    post functionality

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 
 
+// function to validate the animal fields
+function validateAnimal(animal){
+  if(!animal.name || typeof animal.name !== 'string'){
+    return false;
+  }
+  if(!animal.species || typeof animal.species !== 'string'){
+    return false;
+  }
+  if(!animal.diet || typeof animal.diet !== 'string'){
+    return false;
+  }
+  if(!animal.personalityTraits || typeof animal.personalityTraits !== 'string'){
+    return false;
+  }
+  return true;
+}
+
+// function to create new animals from teh POST listener
+function  createNewAnimal(body, animalsArray){
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({animals: animalsArray}, null, 2)
+  );
+
+  return body;
+}
 
 // filter reaults by query
 function filterByQuery(query, animalsArray){
@@ -53,6 +87,10 @@ function findById(id, animalsArray){
 
 
 // route listeners
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query){
@@ -69,6 +107,22 @@ app.get('/api/animals/:id', (req, res) => {
     req.setEncoding(404);
   }
 });
+
+app.post('/api/animals', (req, res) => {
+  // set an id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // validate animal
+  if(!validateAnimal(req.body)){
+    res.status(400).send('The animal is not properly formatted');
+  } else {
+    // add animal to json file and animals array
+    const animal = createNewAnimal(req.body, animals);
+    res.json(req.body);
+  }
+
+});
+
 
 //  start the express server
 app.listen(PORT, () => {
